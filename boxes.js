cf1d7eb0f9db33c7030e016d08979fa9c6f5de3a
Daria-Zakharova <dariazakharova.smthelse@gmail.com>
2022-06-05 21:57:08 +0300

    Delete extra comments

--- boxes.js
+++ boxes.js
@@ -1,45 +1,9 @@
-/* 
-count boxNumber
-itemList example: [120wickedWitch, 100magician, 20catButt, 40alice];
-itemNumber += Number.parseInt(itemNumber[i]);
-
-checking for partially filled boxes
-function checks if boxNumber is an integer
-itemNumber % 140 === 0 ? boxNumber = itemNumber % 140 : false;
-
-Checking if there is enough items of on type to make a box
-Number.parseInt(itemList[i]) === 140;
-
-Checking if there is more than 1 box
-Number.parseInt(itemList[i])%140 === 0;
-
-Checking if there is 1box and some items
-add 140ww to boxArray, replace 160ww to 20ww, continue
-
-function boxForm()
-create empty array for box
-for(i = 0; i < itemList.length; i += 1) {
-    switch (itemList[i]%140) {
-    
-        case 'true':
-        for(n = 1; n<=itemList[i]/140; n+=1){
-        
-        }
-    boxArray.push(itemList[i]);
-    itemList[i].pop(itemList[i]);
-    return boxArray;
-
-    case <140
-    }
-}
-
- */
 //const items = ['140 ww', '120 wicked witch', '100 magician', '281 mermaid tail', '20 cat butt', '40 alice', '140 halfling'];
 let items;
 items = prompt('Please enter the list of items, separated with commas, e.g. "140 wicked witch, 5 magician, 146 dire wolf"');
 items = items.split(', ');
 
-let boxes = []; // final result
+let boxes = [];
 let box = [];
 const boxCapacity = 140;
 
@@ -57,6 +21,7 @@ function checkOneItemBoxes(item) {
 function countFullBoxes(item) {
     return boxNumber = Math.floor(Number.parseInt(item) / boxCapacity);
 }
+
 function countRest(item) {
     return rest = Number.parseInt(item) % boxCapacity;
 }
@@ -119,10 +84,10 @@ function formOneItemBoxes(items) {
     return boxes;
 }
 
-
 function addCombinedBoxes(items) {
     formOneItemBoxes(items);
     for (let i = 0; i < items.length; i += 1) {
+
         if (Number.parseInt(items[i]) < countBoxEmptySpace(box)) {
 
             createBox(items[i]);
@@ -151,13 +116,3 @@ function addCombinedBoxes(items) {
     }
     return boxes;
 }
-
-
-
-/* function countBoxes(items) {
-    return createBoxList(items).length;
-} */
-console.log(addCombinedBoxes(items));
-console.log(boxes);
-console.log(box);
-
